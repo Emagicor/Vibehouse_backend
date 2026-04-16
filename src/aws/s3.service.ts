d4915606f76d1061ecb0afdf779f881849f31f5c
Upamanyu24300 <upamanyu24300@iiitnr.edu.in>
2026-04-16 15:25:44 +0530

    s3 issue fix

--- src/aws/s3.service.ts
+++ src/aws/s3.service.ts
@@ -22,13 +22,7 @@ export class S3Service implements OnModuleInit {
     this.region = process.env.AWS_REGION ?? 'ap-south-1';
     this.bucket = process.env.AWS_S3_KYC_BUCKET ?? 'vibehouse-kyc-documents';
 
-    this.s3 = new S3Client({
-      region: this.region,
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
-      },
-    });
+    this.s3 = new S3Client({ region: this.region });
   }
 
   async onModuleInit() {
--- src/aws/textract.service.ts
+++ src/aws/textract.service.ts
@@ -64,21 +64,9 @@ export class TextractService {
     const region = process.env.AWS_REGION ?? 'ap-south-1';
     this.bucket = process.env.AWS_S3_KYC_BUCKET ?? 'vibehouse-kyc-documents';
 
-    this.textract = new TextractClient({
-      region,
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
-      },
-    });
+    this.textract = new TextractClient({ region });
 
-    this.s3 = new S3Client({
-      region,
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
-      },
-    });
+    this.s3 = new S3Client({ region });
 
     this.openai = new OpenAI({
       apiKey: process.env.OPENAI_API_KEY,
